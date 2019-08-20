a9fb1a4e42842d44bff63dab557774621666f2af
Ben Winding <ben.winding@gmail.com>
2019-08-20 17:28:44 +0930

    Include file model in public_api

--- projects/mat-firebase-upload/src/public-api.ts
+++ projects/mat-firebase-upload/src/public-api.ts
@@ -5,3 +5,4 @@
 export * from './lib/lib.module';
 export * from './lib/form-firebase-files/form-firebase-files.component';
 export * from './lib/form-firebase-image/form-firebase-image.component';
+export * from './lib/FormFileObject';
